e15e0501459bcaf4664baeffad51593cc7b9304b
Sean Craig <sean@taskfirst.co.uk>
2012-09-09 19:10:43 +0100

    no offers but single and multiple items
    added tests for single and multiple items but no offers

---
+++ TDDCheckoutKata/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TDDCheckoutKata
+{
+    public class ShoppingCart
+    {
+        public static int Checkout(string basket)
+        {
+            var total = 0;
+            char[] items = basket.ToCharArray();
+
+            foreach (var item in items)
+            {
+                if (item.Equals(Convert.ToChar("A")))
+            {
+                total+= 50;
+            }
+                else if (item.Equals(Convert.ToChar("B")))
+            {
+                total += 30;
+            }
+                else if (item.Equals(Convert.ToChar("C")))
+            {
+                total += 20;
+            }
+                else if (item.Equals(Convert.ToChar("D")))
+            {
+                total += 15;
+            }
+            }
+    return total ;
+
+
+    }
+    }
+}
--- TDDCheckoutKata/TDDCheckoutKataTests.cs
+++ TDDCheckoutKata/TDDCheckoutKataTests.cs
@@ -5,18 +5,32 @@ namespace TDDCheckoutKata
     class TddCheckoutKataTests
     {
         [Test]
-        public void CheckoutBasketContainsASingleATotalShouldBeFifty()
+        [TestCase("",0, TestName="Basket is empty total is 0")]
+        [TestCase("A",50, TestName="Basket contains 'A' total is 50")]
+        [TestCase("B",30, TestName="Basket contains 'B' total is 30")]
+        [TestCase("C",20, TestName="Basket contains 'B' total is 20")]
+        [TestCase("D",15, TestName="Basket contains 'B' total is 15")]
+        public void CheckoutBasketContainsASingleItemTotalsCorrectly(string basket, int expected)
         {
-            //Arrange
-            var basket = "A";
 
             //Act
             var result = ShoppingCart.Checkout(basket);
 
             //Assert
-            Assert.That(result, Is.EqualTo(50));
+            Assert.That(result, Is.EqualTo(expected));
         }
 
+        [Test]
+        [TestCase("AA", 100, TestName = "Basket contains 'AA' total is 100")]
+        public void CheckoutBasketContainsAMultipleItemsButNoffersTotalsCorrectly(string basket, int expected)
+        {
+
+            //Act
+            var result = ShoppingCart.Checkout(basket);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
         
     }
 }
